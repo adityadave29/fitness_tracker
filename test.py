28308c952761f66915916c65ab6b9bec3189a06a
adityadawe <cookedengineer29@gmail.com>
2025-12-18 15:00:48 +0530

    feature swapped - bug fixed

--- test.py
+++
@@ -1,3 +0,0 @@
-import pandas as py
-import numpy as np
-
---
+++ visualize.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+# --------------------------------------------------------------
+# Load data
+# --------------------------------------------------------------
+
+df = pd.read_pickle("data_resampled.pkl")
+
+# --------------------------------------------------------------
+# Plot single columns
+# --------------------------------------------------------------
+
+set_df = df[df["set"] == 1]
+plt.plot(set_df["acc_y"])
+
+# --------------------------------------------------------------
+# Plot all exercises
+# --------------------------------------------------------------
+
+for label in df["label"].unique():
+    subset = df[df["label"] == label]
+    fig, ax = plt.subplots()
+    plt.plot(subset["acc_y"].reset_index(drop=True), label=label)
+    plt.legend()
+    plt.show()
+    
+for label in df["label"].unique():
+    subset = df[df["label"] == label]
+    fig, ax = plt.subplots()
+    plt.plot(subset[:100]["acc_y"].reset_index(drop=True), label=label)
+    plt.legend()
+    plt.show()
+
+# --------------------------------------------------------------
+# Adjust plot settings
+# --------------------------------------------------------------
+
+mpl.style.use("seaborn-v0_8-deep")
+mpl.rcParams["figure.figsize"] = (20,5)
+mpl.rcParams["figure.dpi"] = 100
+
+
+
+# --------------------------------------------------------------
+# Compare medium vs. heavy sets
+# --------------------------------------------------------------
+
+category_df = df[df[label]]
+
+# --------------------------------------------------------------
+# Compare participants
+# --------------------------------------------------------------
+
+
+# --------------------------------------------------------------
+# Plot multiple axis
+# --------------------------------------------------------------
+
+
+# --------------------------------------------------------------
+# Create a loop to plot all combinations per sensor
+# --------------------------------------------------------------
+
+
+# --------------------------------------------------------------
+# Combine plots in one figure
+# --------------------------------------------------------------
+
+
+# --------------------------------------------------------------
+# Loop over all combinations and export for both sensors
+# --------------------------------------------------------------
